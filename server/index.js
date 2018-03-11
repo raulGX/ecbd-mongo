c7aa6d605f5c3680382a301b633cccc659b05e8a
raulpopovici <raul.popovici95@gmail.com>
2018-03-11 22:14:30 +0200

    db

--- server/index.js
+++ server/index.js
@@ -1,29 +1,28 @@
 require("babel-core/register");
 const Koa = require("koa");
 const app = new Koa();
-
+const mongo = require("koa-mongo");
 // x-response-time
 
+app.use(mongo());
 app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set("X-Response-Time", `${ms}ms`);
+  const result = await ctx.mongo
+    .db("ecbd")
+    .collection("users")
+    .insert({ name: "haha" });
+  const userId = result.ops[0]._id.toString();
+  ctx.body = await ctx.mongo
+    .db("ecbd")
+    .collection("users")
+    .find()
+    .toArray();
+  ctx.mongo
+    .db("ecbd")
+    .collection("users")
+    .remove({
+      _id: mongo.ObjectId(userId)
+    });
 });
-
-// logger
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+app.listen(3001, () => {
+  console.log("listening on port 3001");
 });
-
-// response
-
-app.use(async ctx => {
-  ctx.body = "Hello World1";
-});
-
-app.listen(3001);
