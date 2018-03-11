71debbe28c4b99849da20adf0288d343ff2d279d
raulpopovici <raul.popovici95@gmail.com>
2018-03-11 21:53:40 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require("babel-core/register");
+const Koa = require("koa");
+const app = new Koa();
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = "Hello World1";
+});
+
+app.listen(3001);
